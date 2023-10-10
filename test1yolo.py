00d2983a956f940a7708ceb1c911c45f04818d70
XabierBA <xabitic1@gmail.com>
2023-10-11 00:27:38 +0200

    Add: test image added

--- test1yolo.py
+++ test1yolo.py
@@ -7,7 +7,7 @@ model = YOLO("yolov8n.yaml")
 model = YOLO("yolov8n.pt")
 
 #PREDICTIONS
-res = model.predict("test1.jpeg")[0]
+res = model.predict("bus.jpg")[0]
 
 #IMAGE EDIT AND SAVE
 print(res.boxes)
