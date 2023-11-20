a6f8cb5a6d7984cb99dcacbf20b853a4361275c9
XabierBA <=>
2023-11-20 11:12:48 +0100

    Commit final

--- yoloImgPredict.py
+++ yoloImgPredict.py
@@ -7,11 +7,11 @@ model = YOLO("yolov8n.yaml")
 model = YOLO("yolov8n.pt")
 
 #PREDICTIONS
-res = model.predict("bus.jpg")
+res = model.predict("bus.jpg")[0]
 
-#IMAGE EDIT AND SAVE
+"""IMAGE EDIT AND SAVE
 print(res.boxes)
-res = res.plot(line_width=3)
+res = res.plot(line_width=2)
 res = res[:, :, :: -1]
 res = Image.fromarray(res)
 res.save("output1.jpg")
@@ -20,4 +20,4 @@ res.save("output1.jpg")
 path = cv2.imread("output1.jpg")
 cv2.imshow("Result", path)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.destroyAllWindows()"""
