c1574f2f3c32d510a16d15e9cb03bfe2aec64e66
XabierBA <xabitic1@gmail.com>
2025-04-10 16:33:36 +0200

    v2

--- test2yolo.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolov8m.pt")
-
-res = model.track(source = "test.mp4", show = True)
--- yoloRealPredict.py
+++ yoloRealPredict.py
@@ -9,15 +9,23 @@ model = YOLO("yolov8n.pt")
 #PREDICTIONS
 res = model.track(source="0",show=True)
 
-
-# View results
+# El programa marca las manos de las personas en la imagen poniendo puntos en cada falange
 for r in res:
     im_array = r.plot()  # plot a BGR numpy array of predictions
+    for box in r.boxes:  # iterate through detected objects
+        if box.cls == "hand":  # check if the detected object is a hands
+            keypoints = box.keypoints  # get keypoints for the hand
+            for point in keypoints:
+                x, y = int(point[0]), int(point[1])
+                im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+                cv2.circle(im_array, (x, y), 5, (0, 255, 0), -1)  # draw points on each phalanx    im.show()  # show image
     im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
     im.show()  # show image
 
-
-# #IMAGE SHOWING CODE
+# #IMAGE SHOWING CODEsult", res)
 cv2.imshow("Result", res)
+
+
+cv2.destroyAllWindows()
 cv2.waitKey(0)
 cv2.destroyAllWindows()
